a0d267aeb49245b62d5bb9fae685a10d3e31567f
ennovaronenote <ennovaronenote@gmail.com>
2022-05-06 14:53:28 -0500

    Wrap template creation in an authenticated template

--- src/components/CreateTemplate.js
+++ src/components/CreateTemplate.js
@@ -1,11 +1,15 @@
+import { AuthenticatedTemplate } from '@azure/msal-react'
+
 export default function CreateTemplate() {
     return (
         <>
-            <form>
-                <input type="text" name="template-name" placeholder="Template Name" />
-                <input type="text" name="template-group" placeholder="Template Group" />
-                <input type="text" name="template-link" placeholder="Template Link Access (edit, view, both)" />
-            </form>
+            <AuthenticatedTemplate>
+                <form>
+                    <input type="text" name="template-name" placeholder="Template Name" />
+                    <input type="text" name="template-group" placeholder="Template Group" />
+                    <input type="text" name="template-link" placeholder="Template Link Access (edit, view, both)" />
+                </form>
+            </AuthenticatedTemplate>
         </>
     )
 }
