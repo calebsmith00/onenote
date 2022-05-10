9e06d7fd5c9bc17b919634318ff77ed99078c523
ennovaronenote <ennovaronenote@gmail.com>
2022-05-10 11:14:42 -0500

    Create functionality to fetch & display notebooks

--- src/components/Notebooks.js
+++ src/components/Notebooks.js
@@ -1,14 +1,38 @@
 import { getNotebooks } from "../graphRequests"
 import { useMsal } from "@azure/msal-react"
+import { useEffect, useState } from 'react'
 
 export default function Notebooks() {
     const { instance, accounts } = useMsal()
+    const [notebooks, setNotebooks] = useState([])
+
+    useEffect(() => {
+        let notebookResponse = getNotebooks(instance, accounts[0])
+
+        notebookResponse
+            .then(notebook => setNotebooks(notebook))
+            .catch(err => console.error(err))
+    }, [instance, accounts])
+
+    const parseNotebooks = cb => {
+        if (!notebooks) return
+        
+        return notebooks.map(notebook => {
+            return cb(notebook, notebook.sections)
+        })
+    }
 
     return (
         <>  
             <h1>Notebooks</h1>
 
-            <button onClick={() => getNotebooks(instance, accounts[0])}>Retrieve notebooks</button>
+            {
+                parseNotebooks((notebook, sections) => {
+                    return sections.map(section => {
+                        return <p key={section.id}>{notebook.displayName} - {section.displayName}</p>
+                    })
+                })
+            }
         </>
     )
 }
