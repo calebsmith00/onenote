0842399e9c6837a3ade7feab708c3c9e5db96d41
minifiedcode <minifiedcode@gmail.com>
2022-05-17 11:19:54 -0500

    Test backend API

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,6 +1,7 @@
 import { useMsal, AuthenticatedTemplate } from '@azure/msal-react'
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { callMyAPI } from '../graph'
 
 export default function Homepage() {
     const { instance, accounts } = useMsal()
@@ -18,7 +19,9 @@ export default function Homepage() {
 
             <AuthenticatedTemplate>
                 <Link to={`user/${userId}/onenote/notebooks`}>Notebooks</Link>
+
+                <button onClick={() => { callMyAPI({ userId, endpoint: `api/user/${userId}/retrieve-notebooks` }).then(data => data.json()).then(response => console.log(response)) }}>Call it</button>
             </AuthenticatedTemplate>
-        </div>
+        </div >
     )
 }
