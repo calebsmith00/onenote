b7cfbee54538000d52dc3031db3e2ce6b9d23972
minifiedcode <minifiedcode@gmail.com>
2022-05-18 09:33:36 -0500

    Basic state management for template creation

--- src/components/CreateTemplate.js
+++ src/components/CreateTemplate.js
@@ -5,18 +5,25 @@ import './Template/Template.scss'
 
 export default function CreateTemplate() {
     const { instance, accounts } = useMsal()
-    const [template, setTemplate] = useState({})
+    const [inputFields, setInputFields] = useState({})
 
-    const getInputFields = () => {
+    const getInputFields = handleChange => {
         return inputNeeded.map(field => (
-            <input key={field.id} className="template-input" type="text" placeholder={field.placeholder} name={field.name} />
+            <input key={field.id} className="template-input" type="text" placeholder={field.placeholder} name={field.name} onChange={handleChange} />
         ))
+    }
+
+    const handleInputChange = e => {
+        let { name, value } = e.target
+
+        setInputFields({
+            ...inputFields,
+            [name]: value
+        })
     }
 
     const handleSubmit = e => {
         e.preventDefault()
-
-        console.log(e.target.name)
     }
 
     return (
@@ -24,8 +31,8 @@ export default function CreateTemplate() {
             <h1>This is where templates are created</h1>
             <p>A template is a set of data that outlines what training a new-hire needs to take to complete the onboarding process.</p>
             {/* TEMPLATE CREATION FORM */}
-            <form className="template-form" onSubmit={ }>
-                {getInputFields()}
+            <form className="template-form" onSubmit={handleSubmit}>
+                {getInputFields(handleInputChange)}
 
                 <button type="submit">Submit</button>
             </form>
