f5161dd3640f96901d1f258272a1dfea30b2b2cf
Caleb Smith <minifiedcode@gmail.com>
2022-05-20 14:37:11 -0500

    Prettify

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { UnauthenticatedTemplate, useMsal } from '@azure/msal-react'
-import { Routes, Route } from 'react-router-dom'
-import Homepage from './components/Homepage'
-import Notebooks from './components/Notebooks'
-import Notebook from './components/Notebook'
-import Section from './components/Section'
-import CreateNotebook from './components/CreateNotebook'
-import Template from './components/Template/Template'
-import AddTraining from './components/Template/AddTraining'
+import { UnauthenticatedTemplate, useMsal } from "@azure/msal-react";
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./components/Homepage";
+import Notebooks from "./components/Notebooks";
+import Notebook from "./components/Notebook";
+import Section from "./components/Section";
+import CreateNotebook from "./components/CreateNotebook";
+import Template from "./components/Template/Template";
+import AddTraining from "./components/Template/AddTraining";
 
 function SignInButton() {
-  const { instance } = useMsal()
+  const { instance } = useMsal();
 
-  return <button onClick={() => instance.loginPopup()}>Sign In</button>
+  return <button onClick={() => instance.loginPopup()}>Sign In</button>;
 }
 
 function App() {
@@ -22,17 +22,32 @@ function App() {
 
         {/* USER ROUTES */}
         <Route path="user/:userId/onenote/notebooks" element={<Notebooks />} />
-        <Route path="user/:userId/onenote/notebooks/notebook/:notebook" element={<Notebook />} />
-        <Route path="user/:userId/onenote/notebooks/notebook/:notebook/:section" element={<Section />} />
+        <Route
+          path="user/:userId/onenote/notebooks/notebook/:notebook"
+          element={<Notebook />}
+        />
+        <Route
+          path="user/:userId/onenote/notebooks/notebook/:notebook/:section"
+          element={<Section />}
+        />
 
         {/* ADMIN ROUTES */}
         <Route path="admin/onenote/template/create" element={<Template />} />
-        <Route path="admin/onenote/template/add/trainings" element={<AddTraining />} />
-        <Route path="admin/onenote/notebooks/create" element={<CreateNotebook />} />
+        <Route
+          path="admin/onenote/template/add/trainings"
+          element={<AddTraining />}
+        />
+        <Route
+          path="admin/onenote/notebook/create"
+          element={<CreateNotebook />}
+        />
       </Routes>
 
       <UnauthenticatedTemplate>
-        <p>Welcome to the OneNote project. Please <SignInButton /> to continue with your project.</p>
+        <p>
+          Welcome to the OneNote project. Please <SignInButton /> to continue
+          with your project.
+        </p>
       </UnauthenticatedTemplate>
     </div>
   );
--- src/components/CreateNotebook.js
+++ src/components/CreateNotebook.js
@@ -1,52 +1,54 @@
-import { getUsers } from "../graphRequests"
-import { useMsal } from "@azure/msal-react"
-import { useEffect, useState } from "react"
-import { templates } from '../templates'
-import "../styles/Form.scss"
+import { getUsers } from "../graphRequests";
+import { useMsal } from "@azure/msal-react";
+import { useEffect, useState } from "react";
+import { templates } from "../templates";
+import "../styles/Form.scss";
 
 export default function CreateNotebook() {
-    const { instance, accounts } = useMsal()
-    const [users, setUsers] = useState([])
+  const { instance, accounts } = useMsal();
+  const [users, setUsers] = useState([]);
 
-    useEffect(() => {
-        getUsers(instance, accounts[0])
-            .then(response => setUsers(response))
-            .catch(err => console.error(err))
+  useEffect(() => {
+    getUsers(instance, accounts[0])
+      .then((response) => setUsers(response))
+      .catch((err) => console.error(err));
+  }, [instance, accounts]);
 
-    }, [instance, accounts])
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
-    const handleSubmit = e => {
-        e.preventDefault()
-    }
+  return (
+    <>
+      <h1>Notebook Creation</h1>
 
-    return (
-        <>
-            <h1>Notebook Creation</h1>
+      <form onSubmit={handleSubmit}>
+        <span>
+          <label htmlFor="users">Select a user: </label>
+          <select name="users" id="users">
+            {users.map((user) => (
+              <option key={user.id}>
+                {user.displayName} ({user.mail})
+              </option>
+            ))}
+          </select>
+        </span>
 
-            <form onSubmit={handleSubmit}>
-                <span>
-                    <label htmlFor="users">Select a user: </label>
-                    <select name="users" id="users">
-                    {
-                        users.map(user => <option key={user.id}>{user.displayName} ({user.mail})</option>)
-                    }
-                    </select>
-                </span>
+        <br />
 
-                <br />
-                
-                <span>
-                    <label htmlFor="templates">Select a template: </label>
-                    <select name="templates" id="templates">
-                    {
-                        templates.map(template => <option key={template.id}>{template.group}</option>)
-                    }
-                    </select>
-                </span>
+        <span>
+          <label htmlFor="templates">Select a template: </label>
+          <select name="templates" id="templates">
+            {templates.map((template) => (
+              <option key={template.id}>{template.group}</option>
+            ))}
+          </select>
+        </span>
 
-                <button type="submit" className="btn-submit">Create Notebook</button>
-            </form>
-            
-        </>
-    )
+        <button type="submit" className="btn-submit">
+          Create Notebook
+        </button>
+      </form>
+    </>
+  );
 }
