6cec3d416efd6bd368e1ff08e3846d82f92a7121
Caleb Smith <minifiedcode@gmail.com>
2022-05-20 10:26:58 -0500

    Update template creation page to prompt user to add training

--- src/components/Template/CreateTemplate.js
+++ src/components/Template/CreateTemplate.js
@@ -1,37 +1,69 @@
-import { useState } from 'react'
-import './Template.scss'
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Template.scss";
 
 export default function CreateTemplate() {
-    const [template, setTemplate] = useState({})
+  const [template, setTemplate] = useState({});
+  const [templateCreated, setTemplateCreated] = useState(false);
 
-    const handleChange = e => {
-        const { name, value } = e.target
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-        setTemplate({
-            ...template,
-            [name]: value
-        })
-    }
+    setTemplate({
+      ...template,
+      [name]: value,
+    });
+  };
 
-    const handleSubmit = e => {
-        e.preventDefault()
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        const currentSession = sessionStorage.getItem('template') || JSON.stringify("")
+    const currentSession =
+      sessionStorage.getItem("template") || JSON.stringify("");
 
-        sessionStorage.setItem('template', JSON.stringify([
-            ...JSON.parse(currentSession),
-            template
-        ]))
-    }
+    sessionStorage.setItem(
+      "template",
+      JSON.stringify([...JSON.parse(currentSession), template])
+    );
 
-    return (
+    setTemplate({});
+    setTemplateCreated(true);
+  };
+
+  return (
+    <>
+      {templateCreated ? (
         <>
-            <form className="template-form" onSubmit={handleSubmit}>
-                <input type="text" placeholder="Template Name" className="template-input" onChange={handleChange} name="template-title" />
-                <input type="text" placeholder="Number of Sections" className="template-input" onChange={handleChange} name="template-sections" />
-
-                <button type="submit">Submit</button>
-            </form>
+          <p>Nice! Your template has been created.</p>
+          <p>
+            Go ahead and add some&nbsp;
+            <Link to="/admin/onenote/template/add/trainings">
+              <a>trainings.</a>
+            </Link>
+          </p>
         </>
-    )
+      ) : (
+        <>
+          <form className="template-form" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Template Name"
+              className="template-input"
+              onChange={handleChange}
+              name="template-title"
+            />
+            <input
+              type="text"
+              placeholder="Number of Sections"
+              className="template-input"
+              onChange={handleChange}
+              name="template-sections"
+            />
+
+            <button type="submit">Submit</button>
+          </form>
+        </>
+      )}
+    </>
+  );
 }
