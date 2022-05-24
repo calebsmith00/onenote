9e054a56d70741fff48facc0228ddefcd40331cc
minifiedcode <minifiedcode@gmail.com>
2022-05-24 09:42:03 -0500

    More code splitting

--- src/components/Notebook/UserSelect.js
+++ src/components/Notebook/UserSelect.js
@@ -1,13 +1,17 @@
+function Users({ users }) {
+  return users.map((user) => (
+    <option key={user.id} value={user.id}>
+      {user.displayName} ({user.mail})
+    </option>
+  ));
+}
+
 export default function UserSelect({ users, updateUser }) {
   return (
     <span>
       <label htmlFor="users">Select a user: </label>
       <select name="users" id="users" onChange={updateUser}>
-        {users.map((user) => (
-          <option key={user.id} value={user.id}>
-            {user.displayName} ({user.mail})
-          </option>
-        ))}
+        <Users users={users} />
       </select>
     </span>
   );
