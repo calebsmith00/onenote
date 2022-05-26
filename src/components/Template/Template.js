7d6040dc5746dafba99dfe9cacd07930aed4c07f
minifiedcode <minifiedcode@gmail.com>
2022-05-26 08:43:08 -0500

    Break up useEffect hook into readable chunks

--- src/components/Template/Template.js
+++ src/components/Template/Template.js
@@ -11,17 +11,43 @@ export default function Template() {
   const { instance, accounts } = useMsal();
   const { templateName } = useParams();
 
-  useEffect(() => {
-    getNotebook(instance, accounts[0], templateName).then((notebook) => {
-      if (!notebook[0]) return;
-      const sections = notebook[0].sections;
-
-      sections.map((section) => {
-        if (section.displayName !== templateName) return;
-        getSectionPages(instance, accounts[0], section.id);
-      });
+  const getSectionId = (sections) => {
+    const filteredSections = sections.filter((section) => {
+      if (section.displayName !== templateName) return false;
+      return section;
     });
-  }, [instance, accounts, templateName]);
+
+    if (filteredSections.length > 0) return filteredSections[0].id;
+  };
+
+  const getPageId = (pages) => {
+    const filteredPages = pages.filter((page) => {
+      if (page.title !== "Trainings") return false;
+      return page;
+    });
+
+    if (filteredPages.length > 0) return filteredPages[0].id;
+  };
+
+  const getElements = async () => {
+    try {
+      const notebook = await getNotebook(instance, accounts[0], templateName);
+      const pages = await getSectionPages(
+        instance,
+        accounts[0],
+        getSectionId(notebook.sections)
+      );
+      const page = await getPage(instance, accounts[0], getPageId(pages));
+
+      return page;
+    } catch (err) {
+      console.error(`ERROR: ${err}`);
+    }
+  };
+
+  useEffect(() => {
+    getElements().then((doc) => doc);
+  }, [instance, accounts]);
 
   return <div>ff</div>;
 }
