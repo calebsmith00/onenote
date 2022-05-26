5d964f5782515db1bc8e724d57909f2bcd551467
minifiedcode <minifiedcode@gmail.com>
2022-05-26 08:43:26 -0500

    Add error handling to graph request

--- src/requests/graph/index.js
+++ src/requests/graph/index.js
@@ -8,20 +8,23 @@ export const callMsGraph = async ({
   method = "GET",
   body = undefined,
 }) => {
-  const headers = new Headers();
-  const bearer = `Bearer ${accessToken}`;
+  try {
+    const headers = new Headers();
+    const bearer = `Bearer ${accessToken}`;
 
-  headers.append("Authorization", bearer);
-  headers.append("Content-Type", contentType);
+    headers.append("Authorization", bearer);
+    headers.append("Content-Type", contentType);
 
-  const options = {
-    method,
-    headers: headers,
-    body,
-  };
+    const options = {
+      method,
+      headers: headers,
+      body,
+    };
 
-  if (base === "userSelector") base = `${graphConfig.graphUserEndpoint}`;
-  return fetch(`${base}${endpoint}`, options)
-    .then((response) => response)
-    .catch((err) => err);
+    if (base === "userSelector") base = `${graphConfig.graphUserEndpoint}`;
+
+    return await fetch(`${base}${endpoint}`, options);
+  } catch (err) {
+    console.error(`ERROR: ${err}`);
+  }
 };
