a15a4645cde724bcd8bc0ba211a58f6a706124d4
minifiedcode <minifiedcode@gmail.com>
2022-05-26 09:07:19 -0500

    Add a hook that pulls HTML from template page

--- src/components/Template/Template.js
+++ src/components/Template/Template.js
@@ -1,53 +1,7 @@
-import {
-  getNotebook,
-  getSectionPages,
-  getPage,
-} from "../../requests/exports.js";
-import { useMsal } from "@azure/msal-react";
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useTemplateHTML } from "../../hooks/useTemplateSession";
 
 export default function Template() {
-  const { instance, accounts } = useMsal();
-  const { templateName } = useParams();
+  const templateHTML = useTemplateHTML();
 
-  const getSectionId = (sections) => {
-    const filteredSections = sections.filter((section) => {
-      if (section.displayName !== templateName) return false;
-      return section;
-    });
-
-    if (filteredSections.length > 0) return filteredSections[0].id;
-  };
-
-  const getPageId = (pages) => {
-    const filteredPages = pages.filter((page) => {
-      if (page.title !== "Trainings") return false;
-      return page;
-    });
-
-    if (filteredPages.length > 0) return filteredPages[0].id;
-  };
-
-  const getElements = async () => {
-    try {
-      const notebook = await getNotebook(instance, accounts[0], templateName);
-      const pages = await getSectionPages(
-        instance,
-        accounts[0],
-        getSectionId(notebook.sections)
-      );
-      const page = await getPage(instance, accounts[0], getPageId(pages));
-
-      return page;
-    } catch (err) {
-      console.error(`ERROR: ${err}`);
-    }
-  };
-
-  useEffect(() => {
-    getElements().then((doc) => doc);
-  }, [instance, accounts]);
-
-  return <div>ff</div>;
+  return <div>{templateHTML && templateHTML}</div>;
 }
--- src/hooks/useTemplateSession.js
+++ src/hooks/useTemplateSession.js
@@ -1,4 +1,12 @@
 import { useEffect, useState } from "react";
+import { useMsal } from "@azure/msal-react";
+import { useParams } from "react-router-dom";
+import { getNotebook, getSectionPages, getPage } from "../requests/exports.js";
+
+/*
+ * PURPOSE: gather the list of templates from session storage,
+ * then return that as part of our custom hook.
+ */
 
 export const useTemplateSession = () => {
   const [templates, setTemplates] = useState([]);
@@ -10,4 +18,54 @@ export const useTemplateSession = () => {
   }, []);
 
   return templates;
+};
+
+/*
+ * PURPOSE: Gather HTML from template page within OneNote
+ */
+
+export const useTemplateHTML = () => {
+  const { instance, accounts } = useMsal();
+  const { templateName } = useParams();
+  const [templateHTML, setTemplateHTML] = useState();
+
+  useEffect(() => {
+    const getSectionId = (sections) => {
+      const filteredSections = sections.filter((section) => {
+        if (section.displayName !== templateName) return false;
+        return section;
+      });
+
+      if (filteredSections.length > 0) return filteredSections[0].id;
+    };
+
+    const getPageId = (pages) => {
+      const filteredPages = pages.filter((page) => {
+        if (page.title !== "Trainings") return false;
+        return page;
+      });
+
+      if (filteredPages.length > 0) return filteredPages[0].id;
+    };
+
+    const getElements = async () => {
+      try {
+        const notebook = await getNotebook(instance, accounts[0], templateName);
+        const pages = await getSectionPages(
+          instance,
+          accounts[0],
+          getSectionId(notebook.sections)
+        );
+        const page = await getPage(instance, accounts[0], getPageId(pages));
+
+        setTemplateHTML(page);
+      } catch (err) {
+        console.error(`ERROR: ${err}`);
+      }
+    };
+
+    getElements();
+  }, [instance, accounts, templateName]);
+
+  return templateHTML;
 };
